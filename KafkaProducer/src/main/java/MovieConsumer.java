9d2a6edfb4ca466da26c84ac3403c36e4e8934a5
rob737 <robinsrivastava737@gmail.com>
2025-04-15 07:33:51 +0530

    Kafka consumer

--- KafkaProducer/src/main/java/MovieConsumer.java
+++ KafkaProducer/src/main/java/MovieConsumer.java
@@ -8,4 +8,18 @@ public class MovieConsumer {
     * 2. committed position : It stores the offset which is used as a checkpoint in case consumer crashes and on
     * getting reinstated can start consuming from this position (commitSync and commitAsync methods can be used for this)
     * */
+
+    /*
+    * Each consumer is part of a consumer group identified by group.id.
+    * No two consumers from the same consumer group will consume messages from the same partition of a topic
+    * simultaneously.
+    * Consumers first needs to subscribe to a topic where in they are assigned a consumer group and then they
+    * poll the subscribed topic.
+    *
+    * After subscribing to a set of topics, the consumer will automatically join the group when poll(Duration) is invoked.
+    *
+    * Underneath the covers, the consumer sends periodic heartbeats to the server.
+    * If the consumer crashes or is unable to send heartbeats for a duration of session.timeout.ms,
+    * then the consumer will be considered dead and its partitions will be reassigned to some other consumers within the same consumer group.
+    * */
 }
